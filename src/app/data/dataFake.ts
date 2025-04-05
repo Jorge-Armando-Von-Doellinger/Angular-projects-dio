67259652ef55cc2fb0d995cd243820e11bf14867
Jorge Armando <jorgearmandovdj@gmail.com>
2025-04-05 16:16:19 -0300

    update...

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,61 @@
+export const dataFake = [
+
+  {
+    "id": "1",
+    "image": "../assets/images/sonic-3.jpg",
+    "title": "Filme do Sonic recebe mais uma continuação, dando mais alegria à quem acompanha essa saga!",
+    "description": "Sonic, uma das melhores obras já feitas, terá mais um filme!"
+  },
+
+  {
+    "id": "2",
+    "image": "../assets/images/batman-movie.jpg",
+    "title": "Filme do batman faz sucesso entre seus fãs!",
+    "description": "The Batman acompanha Bruce Wayne em seu segundo ano como vigilante, enfrentando o Charada e mergulhando nos segredos sombrios de Gotham em um suspense cheio de mistério e corrupção."
+  },
+
+  {
+    "id": "3",
+    "image": "../assets/images/black-panther-movie.jpeg",
+    "title": "Pantera negra, o filme",
+    "description": "T'Challa retorna a Wakanda para assumir o trono e enfrentar ameaças internas e externas, enquanto celebra a rica cultura africana e mostra o poder e a inovação de seu reino."
+  },
+
+  {
+    "id": "4",
+    "image": "../assets/images/coringa-movie.jpeg",
+    "title": "Coringa, entre a sanidade e a loucura...",
+    "description": "Arthur Fleck, um homem marginalizado e solitário, sofre um colapso psicológico que o transforma no infame vilão de Gotham, revelando as fragilidades de uma sociedade desigual."
+  },
+
+  {
+    "id": "5",
+    "image": "../assets/images/movie-dune_2021-977931.jpeg",
+    "title": "Dune, um perigo em prol da ciência",
+    "description": "Paul Atreides precisa sobreviver em um planeta desértico repleto de perigos e intrigas, enquanto se envolve em uma guerra pelo controle do recurso mais valioso do universo e descobre seu destino extraordinário."
+  },
+
+  {
+    "id": "6",
+    "image": "../assets/images/spiderman-movie.jpeg",
+    "title": "Homem aranha de volta ao lar, um sucesso entre os adolescentes!",
+    "description": "Após os eventos de Guerra Civil, Peter Parker tenta equilibrar a vida de estudante com suas responsabilidades como Homem-Aranha, enquanto enfrenta o vilão Abutre e prova que está pronto para ser um verdadeiro herói — mesmo sem depender do Homem de Ferro"
+  },
+
+  {
+    "id": "7",
+    "image": "../assets/images/ultimato-movie.jpeg",
+    "title": "Vingadores Ultimato, um sucesso em bilhereria!",
+    "description": "Os heróis sobreviventes se unem para reverter o estilhaço causado por Thanos, em uma batalha épica para restaurar o equilíbrio no universo, culminando em sacrifícios e redefinição do legado dos Vingadores."
+  },
+
+  {
+    "id": "8",
+    "image": "../assets/images/movie-interstellar.jpeg",
+    "title": "Interestelar uma das melhores obras já criadas pelo homem... ",
+    "description": "Em um futuro onde a Terra está morrendo, um grupo de astronautas viaja por um buraco de minhoca em busca de um novo lar para a humanidade. Uma jornada emocionante sobre amor, tempo e sobrevivência além das estrelas."
+  }
+
+
+
+]
